fe456cded6d836b1ff86195548dd44a88bd18cec
Özgür Atak <ozguratak0@gmail.com>
2023-03-06 17:16:07 +0300

    iOS First Commit
    - Creating for iOS App folder and first build.

---
+++ Bartender X/Bartender X/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bartender X
+//
+//  Created by obss on 6.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
